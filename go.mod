24bc53fe47fcb1b252198b23028f5091c42455c0
Henri LEPIC <henrilepic@gmail.com>
2021-09-07 15:31:19 +0200

    create commands.

--- go.mod
+++ go.mod
@@ -15,6 +15,7 @@ require (
 	github.com/go-playground/validator/v10 v10.4.1 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
+	github.com/inconshreveable/mousetrap v1.0.0 // indirect
 	github.com/json-iterator/go v1.1.11 // indirect
 	github.com/leodido/go-urn v1.2.0 // indirect
 	github.com/magiconair/properties v1.8.5 // indirect
@@ -25,6 +26,7 @@ require (
 	github.com/pelletier/go-toml v1.9.3 // indirect
 	github.com/spf13/afero v1.6.0 // indirect
 	github.com/spf13/cast v1.3.1 // indirect
+	github.com/spf13/cobra v1.2.1 // indirect
 	github.com/spf13/jwalterweatherman v1.1.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/subosito/gotenv v1.2.0 // indirect
--- main.go
+++ main.go
@@ -1,45 +1,71 @@
 package main
 
 import (
-	"errors"
-	. "math"
+	"bytes"
+	"fmt"
+	"os/exec"
+	"strings"
+
+	"github.com/spf13/cobra"
 )
 
 var test int = 1
 
 func main() {
-	//
+
+	var cmdBuild = &cobra.Command{
+		Use:   "build",
+		Short: "Build anything to the screen",
+		Long: `echo is for echoing anything back.
+	Echo works a lot like print, except it has a child command.`,
+		Run: func(cmd *cobra.Command, args []string) {
+			if len(args) == 0 {
+				build("./app/main.go", "app/app")
+				build("./auth/main.go", "auth/auth")
+			}
+		},
+	}
+
+	var cmdRun = &cobra.Command{
+		Use:   "run",
+		Short: "Run anything to the screen",
+		Long: `echo is for echoing anything back.
+	Echo works a lot like print, except it has a child command.`,
+		Run: func(cmd *cobra.Command, args []string) {
+			if len(args) == 0 {
+				go run("./app/app")
+				go run("./auth/auth")
+			}
+		},
+	}
+
+	var rootCmd = &cobra.Command{Use: "app"}
+	rootCmd.AddCommand(cmdBuild, cmdRun)
+	rootCmd.Execute()
 }
 
-type MyInt int32
-
-func (mi MyInt) Add(i int) (MyInt, error) {
-	res := int64(mi) + int64(i)
-	if res > MaxInt32 {
-		return -1, errors.New("out of range")
+func build(path, fileName string) error {
+	cmdi := exec.Command("go", "build", "-o "+fileName, path)
+	cmdi.Stdin = strings.NewReader("some input")
+	var out bytes.Buffer
+	cmdi.Stdout = &out
+	err := cmdi.Run()
+	if err != nil {
+		return err
 	}
-	return MyInt(res), nil
+	fmt.Printf("in all caps: %q\n", out.String())
+	return nil
 }
 
-func (mi MyInt) Sub(i int) (MyInt, error) {
-	res := int64(mi) - int64(i)
-	if res < MinInt32 {
-		return -1, errors.New("out of range")
+func run(path string) error {
+	cmdi := exec.Command(path)
+	cmdi.Stdin = strings.NewReader("some input")
+	var out bytes.Buffer
+	cmdi.Stdout = &out
+	err := cmdi.Run()
+	if err != nil {
+		return err
 	}
-	return MyInt(res), nil
-}
-
-func (mi MyInt) Multiply(i int) (MyInt, error) {
-	res := int64(mi) * int64(i)
-	if res > MaxInt32 {
-		return -1, errors.New("out of range")
-	}
-	return MyInt(res), nil
-}
-
-func (mi MyInt) Divide(i int) (MyInt, error) {
-	if i == 0 {
-		return -1, errors.New("try to divide by 0")
-	}
-	return mi / MyInt(i), nil
+	fmt.Printf("in all caps: %q\n", out.String())
+	return nil
 }
