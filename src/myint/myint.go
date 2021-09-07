710c83cbbdf27187be7a909e355db3381d631416
Henri LEPIC <henrilepic@gmail.com>
2021-09-07 15:31:07 +0200

    add myint package.

---
+++ src/myint/myint.go
@@ -0,0 +1,39 @@
+package myint
+
+import (
+	"errors"
+	. "math"
+)
+
+type MyInt int32
+
+func (mi MyInt) Add(i int) (MyInt, error) {
+	res := int64(mi) + int64(i)
+	if res > MaxInt32 {
+		return -1, errors.New("out of range")
+	}
+	return MyInt(res), nil
+}
+
+func (mi MyInt) Sub(i int) (MyInt, error) {
+	res := int64(mi) - int64(i)
+	if res < MinInt32 {
+		return -1, errors.New("out of range")
+	}
+	return MyInt(res), nil
+}
+
+func (mi MyInt) Multiply(i int) (MyInt, error) {
+	res := int64(mi) * int64(i)
+	if res > MaxInt32 {
+		return -1, errors.New("out of range")
+	}
+	return MyInt(res), nil
+}
+
+func (mi MyInt) Divide(i int) (MyInt, error) {
+	if i == 0 {
+		return -1, errors.New("try to divide by 0")
+	}
+	return mi / MyInt(i), nil
+}
