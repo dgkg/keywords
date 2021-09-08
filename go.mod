beaeaa700b2f57e37632eb4c9ab708c6ac921461
Henri LEPIC <henrilepic@gmail.com>
2021-09-08 15:33:05 +0200

    myint: change to convey tests.

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.17
 require (
 	github.com/gbrlsnchs/jwt/v3 v3.0.1
 	github.com/gin-gonic/gin v1.7.4
+	github.com/smartystreets/goconvey v1.6.4
 	github.com/spf13/cobra v1.2.1
 	github.com/spf13/viper v1.8.1
 )
@@ -16,9 +17,11 @@ require (
 	github.com/go-playground/universal-translator v0.17.0 // indirect
 	github.com/go-playground/validator/v10 v10.4.1 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/gopherjs/gopherjs v0.0.0-20181017120253-0766667cb4d1 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/inconshreveable/mousetrap v1.0.0 // indirect
 	github.com/json-iterator/go v1.1.11 // indirect
+	github.com/jtolds/gls v4.20.0+incompatible // indirect
 	github.com/leodido/go-urn v1.2.0 // indirect
 	github.com/magefile/mage v1.9.0 // indirect
 	github.com/magiconair/properties v1.8.5 // indirect
@@ -27,6 +30,7 @@ require (
 	github.com/modern-go/concurrent v0.0.0-20180228061459-e0a39a4cb421 // indirect
 	github.com/modern-go/reflect2 v1.0.1 // indirect
 	github.com/pelletier/go-toml v1.9.3 // indirect
+	github.com/smartystreets/assertions v0.0.0-20180927180507-b2de0cb4f26d // indirect
 	github.com/spf13/afero v1.6.0 // indirect
 	github.com/spf13/cast v1.3.1 // indirect
 	github.com/spf13/jwalterweatherman v1.1.0 // indirect
@@ -34,8 +38,10 @@ require (
 	github.com/subosito/gotenv v1.2.0 // indirect
 	github.com/ugorji/go/codec v1.1.7 // indirect
 	golang.org/x/crypto v0.0.0-20200622213623-75b288015ac9 // indirect
+	golang.org/x/mod v0.4.2 // indirect
 	golang.org/x/sys v0.0.0-20210510120138-977fb7262007 // indirect
 	golang.org/x/text v0.3.5 // indirect
+	golang.org/x/tools v0.1.2 // indirect
 	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
 	google.golang.org/protobuf v1.26.0 // indirect
 	gopkg.in/ini.v1 v1.62.0 // indirect
--- src/myint/myint_test.go
+++ src/myint/myint_test.go
@@ -3,37 +3,66 @@ package myint
 import (
 	"math"
 	"testing"
+
+	. "github.com/smartystreets/goconvey/convey"
 )
 
 func TestAdd(t *testing.T) {
-	data := []struct {
-		title  string
-		value  MyInt
-		param  int
-		should MyInt
-		err    error
-	}{
-		{title: "A", value: 1, param: 1, should: 2, err: nil},
-		{"B", 2, -1, 1, nil},
-		{"C", 0, 0, 0, nil},
-		{"D", 0, math.MaxInt32, math.MaxInt32, nil},
-		{"E", 0, math.MinInt32, math.MinInt32, nil},
-		{"F", 1, math.MaxInt32, -1, ErrOutOfRange},
-		{"G", math.MinInt32, math.MaxInt32, -1, nil},
-	}
-	for _, v := range data {
-		mi := v.value
-		get, err := mi.Add(v.param)
-		if get != v.should {
-			t.Error("for", v.title, "got", mi, "should got", v.should)
-		}
-		if v.err != nil {
-			if err == nil {
-				t.Error("for", v.title, "got nil error should got", v.err.Error())
-			} else if v.err.Error() != err.Error() {
-				t.Error("for", v.title, "got error", err.Error(), "should got", v.err.Error())
-				t.Errorf("value got %v value should %v", get, v.should)
-			}
-		}
-	}
+
+	// Only pass t into top-level Convey calls
+	Convey("Calculate with Add function on MyInt", t, func() {
+		var (
+			i   MyInt = 0
+			res MyInt
+			err error
+		)
+
+		Convey("1+1 should be 2", func() {
+			i = 1
+			res, err = i.Add(1)
+			So(res, ShouldEqual, 2)
+			So(err, ShouldBeNil)
+		})
+
+		Convey("2-1 should be 1", func() {
+			i = 2
+			res, err = i.Add(-1)
+			So(res, ShouldEqual, 1)
+			So(err, ShouldBeNil)
+		})
+
+		Convey("0+math.MaxInt32 should be math.MaxInt32", func() {
+			i = 0
+			res, err = i.Add(math.MaxInt32)
+			So(res, ShouldEqual, math.MaxInt32)
+			So(err, ShouldBeNil)
+		})
+
+		Convey("0+math.MinInt32 should be math.MinInt32", func() {
+			i = 0
+			res, err = i.Add(math.MinInt32)
+			So(res, ShouldEqual, math.MinInt32)
+			So(err, ShouldBeNil)
+		})
+
+		Convey("1+math.MaxInt32 should be -1", func() {
+			i = 1
+			res, err = i.Add(math.MaxInt32)
+			So(res, ShouldEqual, -1)
+			So(err, ShouldEqual, ErrOutOfRange)
+		})
+
+		Convey("math.MinInt32+math.MaxInt32 should be -1", func() {
+			i = math.MinInt32
+			res, err = i.Add(math.MaxInt32)
+			So(res, ShouldEqual, -1)
+			So(err, ShouldBeNil)
+		})
+
+		Convey("should be zero value", func() {
+			var a int = 0
+			So(a, ShouldBeZeroValue)
+		})
+
+	})
 }
